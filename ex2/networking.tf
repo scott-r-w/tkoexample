1bf7f4ff9ae1dcd113be5f858848f39d5acd27e0
Chris Fox <chris.f@quali.com>
2022-06-21 12:27:53 -0700

    ex2

--- ex2/networking.tf
+++ ex2/networking.tf
@@ -26,13 +26,7 @@ resource "aws_subnet" "public-sub-2" {
 
 }
 
-resource "aws_subnet" "private-sub" {
-    vpc_id = "${aws_vpc.vpc.id}"
-    cidr_block = "10.0.3.0/24"
-    map_public_ip_on_launch = "false" 
-    availability_zone = "us-west-1a"    
 
-}
 
 resource "aws_internet_gateway" "igw" {
     vpc_id = "${aws_vpc.vpc.id}"
@@ -51,44 +45,9 @@ resource "aws_route_table" "public-crt" {
 
 }
 
-resource "aws_route_table" "private-crt" {
-    vpc_id = "${aws_vpc.vpc.id}"
-    
-    route {
-        //associated subnet can reach everywhere
-        cidr_block = "0.0.0.0/0"         //CRT uses this IGW to reach internet
-        gateway_id = "${aws_internet_gateway.igw.id}" 
-    }
-    
 
-}
 
-resource "aws_eip" "nat_eip" {
-  vpc        = true
-  depends_on = [aws_internet_gateway.igw]
-}
 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = "${aws_eip.nat_eip.id}"
-  subnet_id     = "${aws_subnet.public-sub-1.id}"
-  depends_on    = [aws_internet_gateway.igw]
-
-}
-
-resource "aws_route_table" "private-rt" {
-  vpc_id = "${aws_vpc.vpc.id}"
-}
-
-resource "aws_route" "private_nat_gateway" {
-  route_table_id         = "${aws_route_table.private-rt.id}"
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.nat.id}"
-}
-
-resource "aws_route_table_association" "private-crta" {
-  subnet_id      = "${aws_subnet.private-sub.id}"
-  route_table_id = "${aws_route_table.private-rt.id}"
-}
 
 resource "aws_route_table_association" "crta-public-subnet-1"{
     subnet_id = "${aws_subnet.public-sub-1.id}"
